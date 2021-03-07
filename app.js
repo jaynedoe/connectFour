2dc25de31800a6710216881820a276c92727a125
jaynedoe <jaedoyne@hotmail.com>
2021-03-08 07:49:27 +1100

    updated logic

--- app.js
+++ app.js
@@ -1,86 +1,81 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
+  const squares = document.querySelectorAll(".grid div");
+  const result = document.querySelector("#result");
+  const displayCurrentPlayer = document.querySelector("#current-player");
+  let currentPlayer = 1;
+ 
 
-    const squares = document.querySelectorAll(".grid div");
-    const result = document.querySelector("#result");
-    const displayCurrentPlayer = document.querySelector("#current-player");
-    let currentPlayer = 1;
-    let i = 0;
+  //add a button to each square so that it changes it's background to either blue or red
 
-    squares.forEach(element => {
-        element.textContent = i;
-        i++
-    });
+  for (let i = 0; i < squares.length; i++) 
+  
+  (function(index){
 
-    //add a button to each square so that it changes it's background to either blue or red
+    squares[i].addEventListener("click", () => {
 
-    for(let i = 0; i < squares.length; i++){
-        squares[i].addEventListener("click", () => {
+      if(squares[index].classList.contains("available"))
+      {
+        if (currentPlayer === 1) {
+            squares[index].setAttribute("class", "playerOne");
+            squares[index - 7].setAttribute("class", "available");
+            currentPlayer = 2;
+            displayCurrentPlayer.textContent = 2;
+    
+          } else if (currentPlayer === 2) {
+            squares[index].setAttribute("class", "playerTwo");
+            squares[index - 7].setAttribute("class", "available");
+            currentPlayer = 1;
+            displayCurrentPlayer.textContent = 1;
+          }
+      } 
+      else 
+      {
+          alert("You can't go here.");
+      }
+    })
+  })(i);
 
-            if(currentPlayer === 1 && squares[i].className === "available"){
-                
-                squares[i].setAttribute("class", "playerOne");
-     
-                //check for win function
-                checkForWin("playerOne", i);
+  function checkForWin() {
 
-                if(i > 6){
-                    let x = i - 7;
-                    squares[x].setAttribute("class", "available");
-                }     
+    const winningArrays = [
+        [0, 1, 2, 3], [41, 40, 39, 38], [7, 8, 9, 10], [34, 33, 32, 31], [14, 15, 16, 17], [27, 26, 25, 24], [21, 22, 23, 24],
+        [20, 19, 18, 17], [28, 29, 30, 31], [13, 12, 11, 10], [35, 36, 37, 38], [6, 5, 4, 3], [0, 7, 14, 21], [41, 34, 27, 20],
+        [1, 8, 15, 22], [40, 33, 26, 19], [2, 9, 16, 23], [39, 32, 25, 18], [3, 10, 17, 24], [38, 31, 24, 17], [4, 11, 18, 25],
+        [37, 30, 23, 16], [5, 12, 19, 26], [36, 29, 22, 15], [6, 13, 20, 27], [35, 28, 21, 14], [0, 8, 16, 24], [41, 33, 25, 17],
+        [7, 15, 23, 31], [34, 26, 18, 10], [14, 22, 30, 38], [27, 19, 11, 3], [35, 29, 23, 17], [6, 12, 18, 24], [28, 22, 16, 10],
+        [13, 19, 25, 31], [21, 15, 9, 3], [20, 26, 32, 38], [36, 30, 24, 18], [5, 11, 17, 23], [37, 31, 25, 19], [4, 10, 16, 22],
+        [2, 10, 18, 26], [39, 31, 23, 15], [1, 9, 17, 25], [40, 32, 24, 16], [9, 7, 25, 33], [8, 16, 24, 32], [11, 7, 23, 29],
+        [12, 18, 24, 30], [1, 2, 3, 4], [5, 4, 3, 2], [8, 9, 10, 11], [12, 11, 10, 9], [15, 16, 17, 18], [19, 18, 17, 16],
+        [22, 23, 24, 25], [26, 25, 24, 23], [29, 30, 31, 32], [33, 32, 31, 30], [36, 37, 38, 39], [40, 39, 38, 37], [7, 14, 21, 28],
+        [8, 15, 22, 29], [9, 16, 23, 30], [10, 17, 24, 31], [11, 18, 25, 32], [12, 19, 26, 33], [13, 20, 27, 34]
+        ];
 
-                currentPlayer = 2;
-                displayCurrentPlayer.textContent = 2;
+    for (let i = 0; i < winningArrays.length; i++) {
+      const squareOne = winningArrays[i][0];
+      const squareTwo = winningArrays[i][1];
+      const squareThree = winningArrays[i][2];
+      const squareFour = winningArrays[i][3];
 
-            } else if(currentPlayer === 2 && squares[i].className === "available"){
-                
-                squares[i].setAttribute("class", "playerTwo");
+      if (
+        squares[squareOne].className === "playerOne" &&
+        squares[squareTwo].className === "playerOne" &&
+        squares[squareThree].className === "playerOne" &&
+        squares[squareFour].className === "playerOne"
+      ) {
+          result.textContent = "Red Won!";
+      } else if(
+          squares[squareOne].className === "playerTwo" &&
+          squares[squareTwo].className === "playerTwo" &&
+          squares[squareThree].className === "playerTwo" &&
+          squares[squareFour].className === "playerTwo"
+      ){
+          result.textContent = "Blue Won!"
+      }
+    }
+  }
 
-                //check for win function
-                checkForWin("playerTwo", i);
-
-
-                if(i > 6){
-                    let x = i - 7;
-                    squares[x].setAttribute("class", "available");
-                }
-
-                currentPlayer = 1;
-                displayCurrentPlayer.textContent = 1;
-
-            }
-        })
-
-
-        function checkForWin(player, squareNumber){
-            console.log(squareNumber);
-            let hWin = true;
-            let vWin = false;
-            let dWin = false;
-            let nextSquareNumber = squareNumber;
-            nextSquareNumber++;
-            console.log(player);
-            console.log(nextSquareNumber);
-
-            //check for horizontal win
-
-            for(i = 0; i < 3; i++){
-
-                if(squares[nextSquareNumber].contains != player){
-                    hWin = false;
-                }
-                nextSquareNumber++;
-            }
-
-
-
-           //check for vertical win
-
-            //check for diagnonal win
-
-            if(hWin || vWin || dWin){
-                alert(`Player ${currentPlayer} wins!`);
-            }
-
-        }
-    };
+  squares.forEach(element => element.addEventListener("click", checkForWin));
+    
 });
+
+
